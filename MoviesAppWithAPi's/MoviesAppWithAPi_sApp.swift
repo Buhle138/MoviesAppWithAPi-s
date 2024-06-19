4a2b2629d71ad1c92da55326e87378acf39f80fb
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-19 16:04:44 +0200

    Initial Commit

---
+++ MoviesAppWithAPi's/MoviesAppWithAPi_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppWithAPi_sApp.swift
+//  MoviesAppWithAPi's
+//
+//  Created by Buhle Radzilani on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppWithAPi_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
